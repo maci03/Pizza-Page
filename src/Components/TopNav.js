413dbc7f443254dd85edd626d207f51196490521
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-27 19:58:10 +0400

    importing icons

--- src/Components/TopNav.js
+++ src/Components/TopNav.js
@@ -1,14 +1,12 @@
 import React from "react";
-
+import { AiOutlineMenu, AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
 const TopNav = () => {
-    return(
-        <div className="max-w-[1520] mx-auto flex justify-beetwen item-center p-4">
-            <div className="flex items-center">
-                <div className="cursor-pointer">
-                    
-                </div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="max-w-[1520] mx-auto flex justify-beetwen item-center p-4">
+      <div className="flex items-center">
+        <div className="cursor-pointer"></div>
+      </div>
+    </div>
+  );
+};
 export default TopNav;
