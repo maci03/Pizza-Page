0cc383327fc5fa464ace96cf3719232367870bc9
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-27 21:50:36 +0400

    cart button

--- src/Components/TopNav.js
+++ src/Components/TopNav.js
@@ -1,31 +1,36 @@
 import React from "react";
 import { AiOutlineMenu, AiOutlineSearch, AiOutlineClose, AiFillTag} from "react-icons/ai";
+import {BsFillCartFill, BsPerson} from 'react-icons/bs'
+
 const TopNav = () => {
   return (
-    <div className="max-w-[1520] mx-auto flex justify-beetwen item-center p-4">
-      <div className="flex items-center">
-        <div className="cursor-pointer">
-            <AiOutlineMenu size={25}/>
-        </div>
-        <h1 className="text-2xl smtext-3xl lg:text-4xl px-2">
-            Yum
-            <span>Eats</span>
-        </h1>
-        <div className="hidden lg:flex item-center bg-gray-200 rounded-full p-1 text-[14px]">
-            <p className="bg-orange-700 text-white rounded-full p-2 text-bold">Free</p>
-            <p className="p-2 text-bold">Delivery</p>
-        </div>
-        <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
-            <AiOutlineSearch />
-            <input className="bg-transparent p-2 w-full focus:outline-none" 
-                type="text"
-                placeholder="Search meal"
-            />
-        </div>
-        <button className="bg-orange-700 text-white hidden md: flex item-center py-2 rounded-full">
-            Cart
-        </button>
+    <div className="max-w-[1520px] mx-auto flex justify-between items-center p-4">
+    <div className="flex items-center">
+      <div className="cursor-pointer">
+        <AiOutlineMenu size={25} />
       </div>
+      <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
+        Yum
+        <span className="font-bold">Eats</span>
+      </h1>
+      <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
+        <p className="bg-orange-700 text-white rounded-full p-2 text-bold">
+          Free
+        </p>
+        <p className="p-2 text-bold">Delivery</p>
+      </div>
+    </div>
+    <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+      <AiOutlineSearch size={25} />
+      <input
+        className="bg-transparent p-2 w-full focus:outline-none"
+        type="text"
+        placeholder="search meals"
+      />
+    </div>
+    <button className="bg-orange-700 text-white hidden md:flex items-center py-2 rounded-full">
+      <BsFillCartFill size={20} /> Cart
+    </button>
     </div>
   );
 };
