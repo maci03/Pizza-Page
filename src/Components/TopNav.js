4571ae86003a0c0f43fbc162956780df3c5d44ef
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-27 19:51:25 +0400

    writing divs for topnav

--- src/Components/TopNav.js
+++ src/Components/TopNav.js
@@ -3,7 +3,11 @@ import React from "react";
 const TopNav = () => {
     return(
         <div className="max-w-[1520] mx-auto flex justify-beetwen item-center p-4">
-
+            <div className="flex items-center">
+                <div className="cursor-pointer">
+                    
+                </div>
+            </div>
         </div>
     )
 }
