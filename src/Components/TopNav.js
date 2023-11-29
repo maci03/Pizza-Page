f4123faa47af1e580d67b346204bdd61c79f3e7b
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-29 12:53:33 +0400

    menu nav list

--- src/Components/TopNav.js
+++ src/Components/TopNav.js
@@ -3,10 +3,12 @@ import {
   AiOutlineMenu,
   AiOutlineSearch,
   AiOutlineClose,
-  AiFillTag,
 } from "react-icons/ai";
 
 import { BsFillCartFill, BsPerson } from "react-icons/bs";
+import { TbTruckReturn } from "react-icons/tb";
+import { FaGoogleWallet } from "react-icons/fa";
+import { MdHelp, MdOutLineFavorite } from "react-icons/md";
 
 const TopNav = () => {
   const [sideNav, setSideNav] = useState(false);
@@ -58,13 +60,45 @@ const TopNav = () => {
           size={25}
           className="absolute right-4 top-4 cursor-pointer"
         />
-        <h2 className="text-2xl p-4">Yum <span className="text-orange-700 font-bold">Eats</span></h2>
+        <h2 className="text-2xl p-4">
+          Yum <span className="text-orange-700 font-bold">Eats</span>
+        </h2>
         <nav>
           <ul className="flex flex-col p-4 text-gray-900">
             <li className="text-xl py-4 flex">
-              <BsPerson size={25} 
-              className="mr-4 text-white bg-black rounded-full"
+              <BsPerson
+                size={25}
+                className="mr-4 text-white bg-black rounded-full"
               />
+              My Account
+            </li>
+            <li className="text-xl py-4 flex">
+              <TbTruckReturn
+                size={25}
+                className="mr-4 text-white bg-black rounded-full"
+              />
+              Delivery
+            </li>
+            <li className="text-xl py-4 flex">
+              <MdOutLineFavorite
+                size={25}
+                className="mr-4 text-white bg-black rounded-full"
+              />
+              My Favourite
+            </li>
+            <li className="text-xl py-4 flex">
+              <FaGoogleWallet
+                size={25}
+                className="mr-4 text-white bg-black rounded-full"
+              />
+              Help
+              </li>
+            <li className="text-xl py-4 flex">
+              <MdHelp
+                size={25}
+                className="mr-4 text-white bg-black rounded-full"
+              />
+              Help
             </li>
           </ul>
         </nav>
