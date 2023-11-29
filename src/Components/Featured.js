9fd48272ff7242d91fd6d705d43df7a25adfffb9
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-29 18:23:59 +0400

    right side

--- src/Components/Featured.js
+++ src/Components/Featured.js
@@ -17,7 +17,7 @@ const Featured = () => {
     },
   ];
 
-  const [currentIndex, setCurrentIndex] = useState(2);
+  const [currentIndex, setCurrentIndex] = useState(0);
   return (
     <div className="max-2-[1520px] h-[500px] w-full py-4 px-4 relative">
       <div className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
@@ -25,6 +25,9 @@ const Featured = () => {
       </div>
       <div className="absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer">
         <BsChevronCompactLeft />
+      </div>
+      <div className="absolute top-[50%] -translate-x-0 translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer">
+        <BsChevronCompactRight />
       </div>
     </div>
   );
