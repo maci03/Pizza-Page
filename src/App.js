2810437c8b084ac236e0baee6de43db3d212a67b
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-29 14:22:07 +0400

    imported featured to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import TopNav from "./Components/TopNav";
+import Featured from "./Components/Featured";
+
 
 function App() {
   return (
     <div className="App">
       <TopNav />
+      <Featured />
     </div>
   );
 }
