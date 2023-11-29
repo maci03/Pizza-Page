b46a5c813b6c684145f434a982eaecea01465d20
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-29 14:27:00 +0400

    array of images

--- src/Components/Featured.js
+++ src/Components/Featured.js
@@ -1,6 +1,19 @@
 import React from "react";
 
 const Featured = () => {
+    const sliders = [
+       
+        {
+            url: 'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672672076/NetflixApp/burger_emxbtv.jpg'
+          },
+          {
+            url: 'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672672452/NetflixApp/pizza_osjb4f.jpg'
+          },
+          {
+            url: 'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672672612/NetflixApp/ric_a4ewxo.jpg',
+          },
+      
+        ]
     return(
         <div>Featured</div>
     )
