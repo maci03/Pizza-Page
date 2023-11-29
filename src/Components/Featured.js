d4ac4f97dea3dd58333ec27387e10cd3eba52e9f
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-29 14:38:36 +0400

    added background img

--- src/Components/Featured.js
+++ src/Components/Featured.js
@@ -2,23 +2,26 @@ import { useState } from "react";
 import React from "react";
 
 const Featured = () => {
-    const sliders = [
-       
-        {
-            url: 'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672672076/NetflixApp/burger_emxbtv.jpg'
-          },
-          {
-            url: 'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672672452/NetflixApp/pizza_osjb4f.jpg'
-          },
-          {
-            url: 'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672672612/NetflixApp/ric_a4ewxo.jpg',
-          },
-        ]
+  const sliders = [
+    {
+      url: "https://res.cloudinary.com/ehizeex-shop/image/upload/v1672672076/NetflixApp/burger_emxbtv.jpg",
+    },
+    {
+      url: "https://res.cloudinary.com/ehizeex-shop/image/upload/v1672672452/NetflixApp/pizza_osjb4f.jpg",
+    },
+    {
+      url: "https://res.cloudinary.com/ehizeex-shop/image/upload/v1672672612/NetflixApp/ric_a4ewxo.jpg",
+    },
+  ];
 
-        const [currentIndex, setCurrentIndex] = useState(0)
-    return(
-        <div></div>
-    )
-}
+  const [currentIndex, setCurrentIndex] = useState(0);
+  return (
+    <div className="max-2-[1520px] h-[500px] w-full py-4 px-4 relative">
+      <div className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+        style={{ backgroundImage: `url(${sliders[currentIndex].url})` }}>
+      </div>
+    </div>
+  );
+};
 
 export default Featured;
