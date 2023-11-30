a61a8357286d23e10c9f65bb996c94ccbc3852a6
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-30 22:15:57 +0400

    header

--- src/Components/Meal.js
+++ src/Components/Meal.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 const Meal = () => {
-  return <div></div>;
+  return (
+    <div className="max-w[1520px] m-auto px-4 py-12">
+      <h1 className="text-orange-500 font-bold text-2xl text-center py-2">
+        Our Meal
+      </h1>
+      <div></div>
+    </div>
+  );
 };
 export default Meal;
