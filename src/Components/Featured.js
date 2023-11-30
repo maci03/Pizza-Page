819f87ad60b085e9ea0a2bf57e0fec7efb28d659
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-30 12:47:39 +0400

    hover for img div

--- src/Components/Featured.js
+++ src/Components/Featured.js
@@ -3,7 +3,6 @@ import React from "react";
 
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 
-
 const Featured = () => {
   const sliders = [
     {
@@ -19,16 +18,17 @@ const Featured = () => {
 
   const [currentIndex, setCurrentIndex] = useState(0);
   return (
-    <div className="max-2-[1520px] h-[500px] w-full py-4 px-4 relative">
-      <div className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
-        style={{ backgroundImage: `url(${sliders[currentIndex].url})` }}>
-      </div>
-      <div className="absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer">
-        <BsChevronCompactLeft />
-      </div>
-      <div className="absolute top-[50%] -translate-x-0 translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer">
-        <BsChevronCompactRight />
-      </div>
+    <div className='max-w-[1540px] h-[500px] w-full m-auto py-4 px-4 relative group'>
+        <div className='w-full h-full rounded-2xl bg-center bg-cover duration-300'
+             style={{backgroundImage: `url(${sliders[currentIndex].url})`}}
+        ></div>
+        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
+            <BsChevronCompactLeft />
+        </div>
+        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
+            <BsChevronCompactRight />
+        </div>
+      
     </div>
   );
 };
