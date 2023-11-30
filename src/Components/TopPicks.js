f72df3d31878737b29c4b3201fca2fb24efd6dd4
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-30 16:33:03 +0400

    styles for toppicks

--- src/Components/TopPicks.js
+++ src/Components/TopPicks.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const TopPicks = () => {
   <>
-    <div className="">
-
-    </div>
+    <h1 className="text-orange-500 font-bold text-2xl text-center py-2">
+      Top Picks
+    </h1>
   </>;
 };
