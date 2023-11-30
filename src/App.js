04f544d48f8c0e15ecaa08f5d4b1d202ecd30334
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-30 16:50:03 +0400

    added return

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import TopNav from "./Components/TopNav";
 import Featured from "./Components/Featured";
 import Delivery from "./Components/Delivery";
-import TopPicks from "./Components/TopPicks"
+import TopPicks from "./Components/TopPicks";
 
 function App() {
   return (
--- src/Components/TopPicks.js
+++ src/Components/TopPicks.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 const TopPicks = () => {
-  <>
-    <h1 className="text-orange-500 font-bold text-2xl text-center py-2">
-      Top Picks
-    </h1>
-  </>;
+  return (
+    <>
+      <h1 className="text-orange-500 font-bold text-2xl text-center py-2">
+        Top Picks
+      </h1>
+    </>
+  );
 };
+export default TopPicks;
