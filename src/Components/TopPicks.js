4969bf5f8aba645f6c2c08c1f272ec2ee46cc32b
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-30 21:49:18 +0400

    item title

--- src/Components/TopPicks.js
+++ src/Components/TopPicks.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { topPicks } from "../data/data";
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/react-splide/css';
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 const TopPicks = () => {
   return (
@@ -10,7 +10,15 @@ const TopPicks = () => {
         Top Picks
       </h1>
       <div className="hidden lg:flex max-w[1520px] m-auto py-2px px-=2">
-
+        {TopPicks.map((item) => {
+          return (
+          <div className="rounded-3xl relative">
+            <div className="absolute w-full h-full bg-black/50 rounded-3xl">
+                <p className="px-2">{item.title}</p>
+            </div>
+            
+          </div>
+        )})}
       </div>
     </>
   );
