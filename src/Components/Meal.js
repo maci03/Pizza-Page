e5f320a182f4a505c0344538a07cf6889d27d795
მაცი <93665709+maci03@users.noreply.github.com>
2023-12-01 20:07:33 +0400

    filter

--- src/Components/Meal.js
+++ src/Components/Meal.js
@@ -2,6 +2,14 @@ import React from "react";
 import { mealData } from "../data/data";
 
 const Meal = () => {
+  const [foods, setFoods] = useState(mealData);
+  const fillterCat = (category) => {
+    setFoods(
+      mealData.filter((item) => {
+        return item.category === category;
+      })
+    );
+  };
   return (
     <div className="max-w[1520px] m-auto px-4 py-12">
       <h1 className="text-orange-500 font-bold text-2xl text-center py-2">
