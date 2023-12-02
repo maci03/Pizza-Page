dae8954050b8be6aa48bdb56b648d07c82c88bd5
მაცი <93665709+maci03@users.noreply.github.com>
2023-12-02 13:43:09 +0400

    newsletter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Featured from "./Components/Featured";
 import Delivery from "./Components/Delivery";
 import TopPicks from "./Components/TopPicks";
 import Meal from "./Components/Meal";
-
+import Categories from "./Components/Categories";
 function App() {
   return (
     <div className="App">
@@ -12,6 +12,7 @@ function App() {
       <Delivery />
       <TopPicks />
       <Meal />
+      <Categories />
     </div>
   );
 }
