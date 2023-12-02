239b213fe1deed8e1dbd641e9044fdb823b5f6b6
მაცი <93665709+maci03@users.noreply.github.com>
2023-12-02 12:43:54 +0400

    categories

---
+++ src/Components/Categories.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { categories } from "../data/data";
+
+const Categories = () => {
+        console.log(categories)
+    return(
+        <div className="max-w-[1520px] m-auto px-4 py-4">
+            <h1 className="text-orange-500 font-bold text-2xl text-center py-2">
+                Trending Categories
+            </h1>
+            <div className="grid grid-cols-2 md:grid-cols-6 gap-5 py-5 px-2">
+                {
+                    
+                }
+            </div>
+        </div>
+    )
+}
