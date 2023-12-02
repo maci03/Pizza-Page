234c26b78bc37b16febe879ab8facd5544f7657b
მაცი <93665709+maci03@users.noreply.github.com>
2023-12-02 14:33:37 +0400

    added divs to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,6 +8,18 @@ import {
 } from "react-icons/fa";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="max-w-[1520px] m-auto px-4 py-2 bg-[#24262b]">
+      <div className="py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
+        <div>
+          <h1 className="w-full text-3xl font-bold text-orange-500">YumEats</h1>
+          <p>
+            The Leopard 2A7 features the latest generation of passive armour and
+            belly armour providing protection against mines and IEDs.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Footer;
